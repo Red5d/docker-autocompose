7b616a0a41a6bd323686b17cde0b307a17b20bc3
Red5d <daniel.colson99@gmail.com>
2016-01-02 23:54:07 -0500

    Fixed actual script not being included and upped version number to account for change.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "docker-autocompose",
-    version = "1.0",
+    version = "1.0.1",
     description = "Generate a docker-compose yaml definition from a running container",
     url = "https://github.com/Red5d/docker-autocompose",
     author = "Red5d",
@@ -9,6 +9,7 @@ setup(
     keywords = "docker yaml container",
     packages = find_packages(),
     install_requires = ['pyaml>=15.8.2', 'docker-py>=1.6.0'],
+    scripts = ['autocompose.py'],
     entry_points={
         'console_scripts': [
             'autocompose = autocompose:main',
