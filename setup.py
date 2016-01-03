0f1a9e9308512728491478e1d291580f2bc06977
Red5d <daniel.colson99@gmail.com>
2016-01-02 23:11:51 -0500

    Fix setup file and reformat autocompose to fit setup requirements.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ setup(
     name = "docker-autocompose",
     version = "1.0",
     packages = find_packages(),
-    install_requires = ['pyaml, docker-py'],
+    install_requires = ['pyaml>=15.8.2', 'docker-py>=1.6.0'],
     entry_points={
         'console_scripts': [
-            'autocompose = autocompose',
+            'autocompose = autocompose:main',
         ]
     }
 )
