fdc4c2a71d3b35804a1117377ba2308654365e58
Red5d <daniel.colson99@gmail.com>
2016-01-02 23:24:06 -0500

    Added additional info to setup file.

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 setup(
     name = "docker-autocompose",
     version = "1.0",
+    description = "Generate a docker-compose yaml definition from a running container",
+    url = "https://github.com/Red5d/docker-autocompose",
+    author = "Red5d",
+    license = "GPLv2",
+    keywords = "docker yaml container",
     packages = find_packages(),
     install_requires = ['pyaml>=15.8.2', 'docker-py>=1.6.0'],
     entry_points={
