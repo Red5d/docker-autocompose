800b088cea8f3d3e239b07d0bfb9e13a1a18b5e1
Red5d <daniel.colson99@gmail.com>
2018-07-27 20:21:35 -0400

    Update setup script to use new docker module, bump minimum module versions, and overall app version.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 setup(
     name = "docker-autocompose",
-    version = "1.0.1",
+    version = "1.2.0",
     description = "Generate a docker-compose yaml definition from a running container",
     url = "https://github.com/Red5d/docker-autocompose",
     author = "Red5d",
     license = "GPLv2",
     keywords = "docker yaml container",
     packages = find_packages(),
-    install_requires = ['pyaml>=15.8.2', 'docker-py>=1.6.0'],
+    install_requires = ['pyaml>=17.12.1', 'docker>=3.4.1'],
     scripts = ['autocompose.py'],
     entry_points={
         'console_scripts': [
